6b17f57959e266c3f57d84e1c7ebd90d9eb0bd8d
HembromRahul <hembromrahul@hotmail.com>
2026-02-17 16:43:38 +0530

    Fixed PWA caching permanently

--- vite.config.js
+++ vite.config.js
@@ -8,30 +8,30 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
-      devOptions: {
-        enabled: true
+      workbox: {
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
       },
       manifest: {
         name: "Flow",
         short_name: "Flow",
-        description: "Minimal smart task manager",
         theme_color: "#000000",
         background_color: "#000000",
         display: "standalone",
-        start_url: "/",
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/flow-192.png",
             sizes: "192x192",
-            type: "image/png"
+            type: "image/png",
           },
           {
-            src: "/icon-512.png",
+            src: "/flow-512.png",
             sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
-    })
-  ]
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
